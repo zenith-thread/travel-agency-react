a965c1e69ea1bb880b28b314824d16d3c0f51ec0
Zenith Hanif <zenithpkhanif@gmail.com>
2025-06-12 11:52:59 +0500

    Adding basic routing

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,8 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, route, layout } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  layout("routes/admin/AdminLayout.tsx", [
+    route("dashboard", "routes/admin/Dashboard.tsx"),
+    route("all-users", "routes/admin/AllUsers.tsx"),
+  ]),
+] satisfies RouteConfig;
---
+++ app/routes/admin/AdminLayout.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router";
+
+const AdminLayout = () => {
+  return (
+    <div className="admin-layout">
+      AdminLayout
+      <aside className="w-full max-w-[270px] hidden lg:block">Sidebar</aside>
+      <aside className="children">
+        <Outlet />
+      </aside>
+    </div>
+  );
+};
+
+export default AdminLayout;
